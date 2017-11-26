a3aef5472dc2ad1d5ff5972e25ccdc4da4bf6b6e
Nikita Bhatia <nikitabhatia.17@gmail.com>
2017-11-26 18:51:20 +1100

    Tried another script for the contact form but it still doesn't work

--- project/fifty_fifty/webcore/templates/feedback/form-to-email.php
+++ project/fifty_fifty/webcore/templates/feedback/form-to-email.php
@@ -1,180 +1,60 @@
 <?php
-if (isset($_POST['submit'])) {
-
-  require_once "Mail.php";
-
-  $host = "ssl://smtp.zoho.com";
-  $username = "mentoring@fifty50.org.au";
-  $password = "fiddycent";
-  $port = "465";
-  $to = 'mentoring@fifty50.org.au';
-  $subject = 'Contact Form - New Response';
-  $message = 'First Name: ' . $_POST['fname'] . "\r\n\r\n";
-  $message .=  'Email: ' . $_POST['email'] . "\r\n\r\n";
-  $message .= 'Message: ' . $_POST['message'];
-  $headers = "From: $email\r\n";
-  $headers .= 'Content-Type: text/plain; charset=utf-8';
-  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-  if ($email){
-    $headers .= "\r\nReply-To: $email";
-  }
-  $smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
-  //$mail = $smtp->send($to, $headers, $subject, $message);
-  $success = mail($to, $subject, $message, $headers, '-fmentoring@fifty50.org.au');
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "Error; you need to submit the form!";
 }
+$name = $_POST['first_name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email))
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'mentoring@fifty50.org.au';//<== update the email address
+$email_subject = "New Form Submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
+
+$to = "mentoring@fifty50.org.au";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank_you.html');
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="img/logo.png">
-
-    <title>Fifty50 Mentoring Program</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href={% static "MenteePage/vendor/bootstrap/css/bootstrap.min.css" %} rel="stylesheet" type="text/css">
-
-    <!-- Custom Fonts -->
-    <link href={% static "MenteePage/vendor/font-awesome/css/font-awesome.min.css" %} rel="stylesheet" type="text/css">
-
-    <!-- Plugin CSS -->
-    <link href={% static "MenteePage/vendor/magnific-popup/magnific-popup.css" %} rel="stylesheet" type="text/css">
-
-    <!-- Custom CSS -->
-    <link href={% static "MenteePage/css/blog-home.css" %} rel="stylesheet" type="text/css">
-
-    <!-- Theme CSS -->
-    <link href={% static "MenteePage/css/creative.min.css" %} rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-
-    <![endif]-->
-
-
-    <style type="text/css">
-        .form {
-            margin-bottom: 60px;
-        }
-
-        input[type=submit] {
-            color: white;
-            background-color: rgba(246, 139, 35, 1);
-            border-radius: 300px;
-            padding: 15px 30px;
-        }
-
-        input[type=submit]:hover {
-            background-color: rgba(189, 24, 34, 1);
-        }
-    </style>
-
-  </head>
-
-  <body>
-    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand page-scroll" href="../menteelogin.html">
-                  Fifty50 Mentoring
-                </a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        {% if request.user.profile.role == 'Mentor' %}
-                        <a class="page-scroll" href="../content.html">Training & Content</a>
-                        {% else %}
-                        <a class="page-scroll" href="../content.html">Content</a>
-                        {% endif %}
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="../blog/post_list.html">News & Events</a>
-                    </li>
-
-                    <li>
-                         {% if request.user.profile.role == 'Mentor' %}
-                         <a class="page-scroll" href="../mentor.html">Mentee Details</a>
-                        {% else %}
-                         <a class="page-scroll" href="../mentor.html">Mentor Details</a>
-                        {% endif %}
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="../resources.html">Resources</a>       <!-- login -->
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="../FAQ.html">FAQ & Help</a>
-                    </li>
-                    <li>
-                        <a class="page-scroll" href="feedback_contact.html">Contact Us</a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Me <img src={% static "accounts/img/unnamed.png" %} style="height:25px;width:25px; border-radius: 50%"> <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a class="page-scroll" href="../profile.html">My Profile</a>
-                            </li>
-                            <li>
-                                <a class="page-scroll" href="../settings.html">Settings</a>
-                            </li>
-                            <li>
-                                <a class="page-scroll" href="{% url 'account_logout' %}">Sign Out</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
-
-    <h2 class="section-heading">Form Submssion</h2>
-
-    <?php if (isset($success) && $success){ ?>
-    <h1> Thank You! </h1>
-    <p> Your message has been sent! An admin will be in touch soon!</p>
-    <?php } else { ?>
-    <h1> Oops! </h1>
-    <p> There was an error in sending your message. Please contact an admin directly at <a href="mailto:mentoring@fifty50.org.au">mentoring@fifty50.org.au</a>
-    </p>
-    <?php } ?>
-
-    {% include 'profile_footer.html' %}
-
-    <script>
-      function showDiv() {
-         document.getElementById('submission_confirmed').style.display = "block";
-      }
-    </script>
-
-    <!-- jQuery -->
-    <script src={% static "MenteePage/vendor/jquery/jquery.min.js" %}></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src={% static "MenteePage/vendor/scrollreveal/scrollreveal.min.js" %}></script>
-    <script src={% static "MenteePage/vendor/magnific-popup/jquery.magnific-popup.min.js" %}></script>
-
-    <!-- Theme JavaScript -->
-    <script src={% static "MenteePage/js/creative.min.js" %}></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src={% static "MenteePage/vendor/bootstrap/js/bootstrap.min.js" %}></script>
-
-  </body>
-</html>
